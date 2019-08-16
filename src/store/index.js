3c9ab1a05ecab50cc3fedc612d8ce7357975bfdc
Vakindu Philliam <Pvakindu@gmail.com>
2019-08-17 00:59:11 +0300

    Add files via upload

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+
+import rootReducer from '../reducers';
+
+
+
+const store = createStore(rootReducer, window.devToolsExtension && window.devToolsExtension());
+
+
+
+export default store;
